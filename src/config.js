638120911eff92534e9e5e1249cc7afe3ba76886
jokessenich <johnny.kessenich@gmail.com>
2019-10-30 16:37:19 -0700

    new db

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://mzpypmbnhxtoxw:4f71a69e58597e8ce28212fad64c74fe78627153675142a82db79e97f79a8738@ec2-174-129-253-101.compute-1.amazonaws.com:5432/d8k2597jhq751d',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://cdhjkykzawzvup:5a5a4b5260d9fba2096f67a2e71829be31f9e2e55e4fb747e3c96e02951169f2@ec2-174-129-231-100.compute-1.amazonaws.com:5432/d6uihs695i7ugr',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:a@localhost/blogful-test'
   }
