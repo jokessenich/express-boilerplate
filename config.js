64ab8a4f18e69dd1c695b7cd01beee8f0c4e19a2
jokessenich <johnny.kessenich@gmail.com>
2019-10-30 21:37:34 -0700

    changed url in config

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DATABASE_URL: process.env.DATABASE_URL || 'postgres://zrwvolqkgzrpfb:709b819f9a8fbefd372b127712acd9aa567739ca9bd1b14f4c318c10d40515c7@ec2-54-235-250-38.compute-1.amazonaws.com:5432/dhn9ucgfee4r9',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgres://cdhjkykzawzvup:5a5a4b5260d9fba2096f67a2e71829be31f9e2e55e4fb747e3c96e02951169f2@ec2-174-129-231-100.compute-1.amazonaws.com:5432/d6uihs695i7ugr',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://dunder_mifflin:a@localhost/blogful-test'
 
   }
